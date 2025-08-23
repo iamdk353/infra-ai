015e82f8ff8b0da0c78f99cf19135a386d2f271d
Dhanush Kumar M <iamdk353@gmail.com>
2025-08-23 13:54:07 +0530

    chat interface ready

--- app/app/ai-chat/page.tsx
+++ app/app/ai-chat/page.tsx
@@ -1,4 +1,49 @@
+"use client";
+import { PlaceholdersAndVanishInput } from "@/components/ui/placeholders-and-vanish-input";
+import {
+  Conversation,
+  ConversationContent,
+  ConversationScrollButton,
+} from "@/components/ui/shadcn-io/ai/conversation";
+import { Message, MessageContent } from "@/components/ui/shadcn-io/ai/message";
+
+import { useState } from "react";
 const page = () => {
-  return <div>page</div>;
+  const placeholders = [
+    "What's the first rule of Fight Club?",
+    "Who is Tyler Durden?",
+    "Where is Andrew Laeddis Hiding?",
+    "Write a Javascript method to reverse a string",
+    "How to assemble your own PC?",
+  ];
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+  };
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("submitted");
+  };
+  const [input, setInput] = useState("");
+  return (
+    <div className="h-full">
+      <Conversation className="relative w-full" style={{ height: "80vh" }}>
+        <ConversationContent>
+          <Message from={"user"}>
+            <MessageContent>Hi there!</MessageContent>
+          </Message>
+          <Message from={"assistant"}>
+            <MessageContent>Hi there!</MessageContent>
+          </Message>
+        </ConversationContent>
+        <ConversationScrollButton />
+      </Conversation>
+      <PlaceholdersAndVanishInput
+        placeholders={placeholders}
+        onChange={handleChange}
+        onSubmit={onSubmit}
+      />
+    </div>
+  );
 };
 export default page;
